bb3f0feb6e123f195bf860e4d91161143e4bc8be
kanedaaaa <samflynn19121@gmail.com>
2024-09-21 01:39:17 +0400

    feat/ user store

--- service/user/store.go
+++ service/user/store.go
@@ -1,6 +1,11 @@
 package user
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/kanedaaaa/auth-go/types"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +13,43 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRow(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.Id == 0 {
+		return nil, fmt.Errorf("user wasn't found")
+	}
+
+	return u, nil
+}
+
+func scanRow(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+
+	err := rows.Scan(
+		&user.Id,
+		&user.Username,
+		&user.Password,
+		&user.Email,
+		&user.CreatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
--- types/types.go
+++ types/types.go
@@ -1,7 +1,17 @@
 package types
 
+import "time"
+
+type User struct {
+	Id        int       `json:"id"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
 type SignupPyaload struct {
-	UserName string `json:"username"`
+	Username string `json:"username"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
